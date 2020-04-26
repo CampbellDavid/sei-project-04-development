e0bf92f30227f8c90a516cda2e2f2d1c46735ec5
CampbellDavid <dcampbell092@gmail.com>
2020-04-26 18:24:27 +0100

    dev

--- frontend/src/components/common/ErrorPage.js
+++ frontend/src/components/common/ErrorPage.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const ErrorPage = () => {
 	return (
-		<body className='has-navbar-fixed-top'>
-			<h1 className='unknown-route'>Error 404: This page does not exist</h1>
-		</body>
+		<div className='bg-set-9'>
+			<div className='bg-grad'>
+				<section
+					className='body-div center-item-screen mt-5'
+					style={{ height: '100vh' }}
+				>
+					<h1 className='font title-head'>
+						Error 404: This page does not exist
+					</h1>
+				</section>
+			</div>
+		</div>
 	)
 }
 
