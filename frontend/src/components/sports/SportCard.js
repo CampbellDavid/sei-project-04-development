603b730ac3f5d2f825deb62bb5ab69b5a3f6b1cf
CampbellDavid <dcampbell092@gmail.com>
2020-04-27 13:23:24 +0100

    dev

--- frontend/src/components/sports/SportCard.js
+++ frontend/src/components/sports/SportCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 
 const SportCard = ({ id, name, image }) => (
-	<Card className='m-3' style={{ width: '13rem', height: '16rem' }}>
+	<Card className='m-3' style={{ width: '13rem' }}>
 		<Card.Img style={{ height: '50%' }} variant='top' src={image} />
 		<Card.Body>
 			<Card.Title className='text-center text-uppercase'>{name}</Card.Title>
