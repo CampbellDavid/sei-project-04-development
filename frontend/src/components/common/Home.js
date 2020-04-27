199fc1b6b5aea19b43d9c2bd6ca570b20bc74443
CampbellDavid <dcampbell092@gmail.com>
2020-04-27 14:10:00 +0100

    dev

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -57,31 +57,33 @@ const Home = () => (
 					</div>
 					<div className='col-xl-5 p-0'>
 						<div className='p-3'>
-							<div className='font text-justify'>
-								<h2>About Us</h2>
-								<p>
-									Aute enim aliquip laboris culpa. Quis voluptate mollit
-									exercitation cupidatat dolore anim velit dolore occaecat quis
-									excepteur. Pariatur sint sint irure ipsum nostrud ad proident
-									anim anim incididunt aliquip mollit esse proident. Dolore
-									adipisicing ut ad eu culpa adipisicing voluptate anim enim eu.
-									Incididunt anim ea tempor proident. Non nisi laboris fugiat
-									incididunt ullamco in aliquip ex enim enim. Culpa cupidatat
-									excepteur commodo sint excepteur sint.
-								</p>
-								<p>
-									Fugiat id exercitation in sit exercitation esse. Esse
-									incididunt culpa sit mollit ipsum dolor dolor. Eu excepteur
-									laboris velit officia deserunt. Aliquip reprehenderit est
-									tempor nostrud mollit culpa et minim cupidatat. Qui velit eu
-									pariatur anim proident veniam.
-								</p>
-								<p>
-									Nisi est do sit velit est ad pariatur occaecat in consequat
-									labore. Sit do laborum adipisicing exercitation fugiat anim
-									tempor occaecat duis voluptate commodo eu. Dolore commodo
-									dolor pariatur elit ea reprehenderit mollit do.
-								</p>
+							<div className='text-justify'>
+								<h2 className='font sub-head'>About Us</h2>
+								<div className='sub-font' style={{ fontSize: '14px' }}>
+									<p>
+										Aute enim aliquip laboris culpa. Quis voluptate mollit
+										exercitation cupidatat dolore anim velit dolore occaecat
+										quis excepteur. Pariatur sint sint irure ipsum nostrud ad
+										proident anim anim incididunt aliquip mollit esse proident.
+										Dolore adipisicing ut ad eu culpa adipisicing voluptate anim
+										enim eu. Incididunt anim ea tempor proident. Non nisi
+										laboris fugiat incididunt ullamco in aliquip ex enim enim.
+										Culpa cupidatat excepteur commodo sint excepteur sint.
+									</p>
+									<p>
+										Fugiat id exercitation in sit exercitation esse. Esse
+										incididunt culpa sit mollit ipsum dolor dolor. Eu excepteur
+										laboris velit officia deserunt. Aliquip reprehenderit est
+										tempor nostrud mollit culpa et minim cupidatat. Qui velit eu
+										pariatur anim proident veniam.
+									</p>
+									<p>
+										Nisi est do sit velit est ad pariatur occaecat in consequat
+										labore. Sit do laborum adipisicing exercitation fugiat anim
+										tempor occaecat duis voluptate commodo eu. Dolore commodo
+										dolor pariatur elit ea reprehenderit mollit do.
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -90,22 +92,27 @@ const Home = () => (
 						<div className='col-xl-8 bg-home'>
 							<div className='p-3'>
 								<div className='font text-justify'>
-									<h3>Featured Event: Yosemite Rock Climbing</h3>
-									<p>
-										Ullamco commodo laboris exercitation irure reprehenderit.
-										Incididunt velit ipsum commodo aliqua ad est tempor ea. Do
-										ea pariatur qui tempor sit veniam laboris. Officia sunt
-										irure veniam occaecat quis mollit deserunt deserunt. Sunt
-										velit adipisicing enim dolor occaecat magna quis dolor
-										aliquip quis Lorem. Laborum occaecat elit consequat laboris
-										consequat consectetur pariatur excepteur do nostrud.
-									</p>
-								</div>
-								<div className='font text-justify'>
-									<p>
-										Est ad excepteur eu ut velit tempor. In commodo adipisicing
-										eu nostrud. Velit et proident aute voluptate aute sunt.
-									</p>
+									<h2 className='font sub-head'>
+										Featured Event: Yosemite Rock Climbing
+									</h2>
+									<div className='sub-font' style={{ fontSize: '14px' }}>
+										<p>
+											Ullamco commodo laboris exercitation irure reprehenderit.
+											Incididunt velit ipsum commodo aliqua ad est tempor ea. Do
+											ea pariatur qui tempor sit veniam laboris. Officia sunt
+											irure veniam occaecat quis mollit deserunt deserunt. Sunt
+											velit adipisicing enim dolor occaecat magna quis dolor
+											aliquip quis Lorem. Laborum occaecat elit consequat
+											laboris consequat consectetur pariatur excepteur do
+											nostrud.
+										</p>
+
+										<p>
+											Est ad excepteur eu ut velit tempor. In commodo
+											adipisicing eu nostrud. Velit et proident aute voluptate
+											aute sunt.
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
